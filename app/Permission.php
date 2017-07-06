fdfd3408393cf607f4f836d47fb76e08a5610322
Nguyen Sanh Toan <toan.nguyensanh@codeandmore.com>
2017-07-06 15:01:20 +0700

    permission manage

--- app/Permission.php
+++ app/Permission.php
@@ -1,8 +1,17 @@
-<?php namespace App;
+<?php 
+namespace App;
 
 use Zizaco\Entrust\EntrustPermission;
 
 class Permission extends EntrustPermission
 {
-	
+	protected $fillable = ['name', 'display_name', 'description'];	
+
+    /**
+     * The roles that belong to the permission.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role', 'permission_role');
+    }
 }
